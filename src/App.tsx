023244097929403a2fe32ae1570ee52a9ed096ac
piz86 <72163038+piz86@users.noreply.github.com>
2026-03-28 17:31:18 -0400

    Add App.tsx file for the main application component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import store from './src/store';
+import AppNavigator from './src/navigation/AppNavigator';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
